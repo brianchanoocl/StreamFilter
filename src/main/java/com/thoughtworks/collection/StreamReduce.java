a0b255810a23b911b5eac90566224bb0609a37e7
brianchanoocl <Delubdi118>
2021-12-01 14:27:48 +0800

    [StreamFilter] [Brian] -feat commit getLastOdd()

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -1,11 +1,12 @@
 package com.thoughtworks.collection;
 
 import java.util.List;
+import java.util.Comparator;
 
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().reduce(0, (returnNumber, number) -> number%2 != 0 ? number : returnNumber);
     }
 
     public String getLongest(List<String> words) {
