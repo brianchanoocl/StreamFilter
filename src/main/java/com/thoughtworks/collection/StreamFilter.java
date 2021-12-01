c78a2f441ba4efae7caaee6fc409556faeeba133
brianchanoocl <Delubdi118>
2021-12-01 11:54:06 +0800

    [StreamFilter] [Brian] -feat commit filterEven()

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class StreamFilter {
 
     public List<Integer> filterEven(List<Integer> numbers) {
-        return null;
+        return numbers.stream().filter(item->item%2==0).collect(Collectors.toList());
     }
 
     public List<String> filterEndWithS(List<String> words) {
